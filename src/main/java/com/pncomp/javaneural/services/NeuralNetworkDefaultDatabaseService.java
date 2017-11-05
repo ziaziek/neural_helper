eb53100ddcae1a4c5f126d1feb2737bf97fe9900
ziaziek <ziaziek@poczta.fm>
2017-11-05 17:23:51 +0100

    Database saving fixed. Reading from database implemented. NamingFactory created.

--- src/main/java/com/pncomp/javaneural/services/NeuralNetworkDefaultDatabaseService.java
+++ src/main/java/com/pncomp/javaneural/services/NeuralNetworkDefaultDatabaseService.java
@@ -4,13 +4,10 @@ import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.neuroph.core.NeuralNetwork;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class NeuralNetworkDefaultDatabaseService implements NetworkSaveReadService {
 
@@ -28,17 +25,23 @@ public class NeuralNetworkDefaultDatabaseService implements NetworkSaveReadServi
         Connection conn = null;
         try {
             conn=createConnection();
+            conn.setAutoCommit(false);
             net.save(defaultFileName);
             log.info("File saved.");
             final String sqlInsertNN = "insert into neural_network(name, date_created, nn) values(?,current_timestamp,?)";
             try(PreparedStatement stmt = conn.prepareStatement(sqlInsertNN)){
+                FileInputStream fis = new FileInputStream(new File(defaultFileName));
                 stmt.setString(1, name);
-                stmt.setBytes(2, net.toString().getBytes());
+                stmt.setBinaryStream(2, fis);
                 //stmt.setObject(2, net);
-                stmt.execute();
+                stmt.executeUpdate();
                 log.info("Neural network saved in database.");
+                conn.commit();
+                fis.close();
             } catch(SQLException ex){
                 log.warn(ex);
+            } catch (IOException e) {
+                log.warn("Could not read");
             }
 
         } catch (ClassNotFoundException e) {
@@ -64,11 +67,43 @@ public class NeuralNetworkDefaultDatabaseService implements NetworkSaveReadServi
 
     @Override
     public NeuralNetwork readNetwork(String networkName) {
-        //first read data from database into a file
-
-        //build network from the file
-
-        //delete the file
+        Connection conn = null;
+        try {
+            conn = createConnection();
+            final String readSql = "select nn from neural_network where name =?";
+            try(PreparedStatement stmt = conn.prepareStatement(readSql)){
+                stmt.setString(1, networkName);
+                try(ResultSet rs = stmt.executeQuery()){
+                    log.info("Neural network retrieved from database.");
+                    while(rs.next()){
+                        log.info("Trying to load network.");
+                        byte[] b = rs.getBytes("nn");
+                        log.info("Network byte length: "+String.valueOf(b.length));
+                        try (ByteArrayInputStream str = new ByteArrayInputStream(b)) {
+                            NeuralNetwork net = NeuralNetwork.load(str);
+                            log.info("Neural network read. Number of weights:"+String.valueOf(net.getWeights().length));
+                            str.close();
+                            log.info("Stream closed. Network has "+String.valueOf(net.getInputsCount()+" inputs and "+String.valueOf(net.getOutputsCount())+" outputs."));
+                            return net;
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if(conn!=null){
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return null;
     }
 }
